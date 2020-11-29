77101dcf2ce1dbee43c034b5865985dbb58a6a76
deepakperumal <a.deepakperumal@gmail.com>
2020-11-29 20:29:51 +0530

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,32 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
-  title = 'social';
+export class AppComponent  {
+  feeds: Array<any>;
+
+  constructor(private http: HttpClient) {}
+
+  ngOnInit() {
+    this.getData();
+  }
+
+
+  getData() {
+    const url =
+      'https://api.bellita.co.in/api/v1/getfeedsgroupby/all/0/n?token=160f7dcb51d7f1df4d2decbe5f0299cd82b552c3&filter={"order":"id desc","limit":"20","skip":"0"} ';
+    this.http.get<any[]>(url).subscribe(
+      (result) => {
+        this.feeds = result['data'];
+        console.log(this.feeds);
+      },
+      (error) => {
+        console.log(error);
+      }
+    );
+  }
 }
